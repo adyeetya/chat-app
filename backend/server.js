0d03fab71d3422e5c08fc06cf8a9526cec83137c
adyeetya <2612adityasingh2000@gmail.com>
2024-06-17 22:28:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 import messageRouter from './routes/message.routes.js'
@@ -10,20 +11,24 @@ import { app, server } from './socket/socket.js'
 dotenv.config()
 const PORT = process.env.PORT || 3000
 
-
+const __dirname = path.resolve()
 
 app.use(express.json()) //to parse the incoming request body with JSON payload
 app.use(cookieParser()) //to parse the cookie in the protect route middleware used for authenticating user
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
 
 app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRouter)
 app.use('/api/users', userRouter)
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
 server.listen(PORT, () => {
-  connectToMongodb()
+  connectToMongodb() 
   console.log(`server is running on port: ${PORT}`)
 })
